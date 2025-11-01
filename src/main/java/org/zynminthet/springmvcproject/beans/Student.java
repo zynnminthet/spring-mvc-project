12d6a503f3696005bedbbbe9d6ce0a0393f79644
zynnminthet <zynnminthet@gmail.com>
2025-11-01 08:42:10 -0700

    chore: no update

--- src/main/java/org/zynminthet/springmvcproject/beans/Student.java
+++ src/main/java/org/zynminthet/springmvcproject/beans/Student.java
@@ -1,4 +1,63 @@
 package org.zynminthet.springmvcproject.beans;
 
+import org.springframework.beans.factory.annotation.Value;
+import org.springframework.stereotype.Component;
+
+import java.util.Map;
+
+import java.util.LinkedHashMap;
+
 public class Student {
+    private String firstName;
+    private String lastName;
+
+    private String country;
+    private String favoriteLanguage;
+
+    public String getFavoriteLanguage() {
+        return favoriteLanguage;
+    }
+
+    public void setFavoriteLanguage(String favoriteLanguage) {
+        this.favoriteLanguage = favoriteLanguage;
+    }
+
+    public String getCountry() {
+        return country;
+    }
+
+    public void setCountry(String country) {
+        this.country = country;
+    }
+
+
+    public Student(String firstName, String lastName, String country) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+        this.country = country;
+    }
+
+    public Student() {
+    }
+
+    public Student(String firstName, String lastName) {
+        this.firstName = firstName;
+        this.lastName = lastName;
+    }
+
+    public String getFirstName() {
+        return firstName;
+    }
+
+    public void setFirstName(String firstName) {
+        this.firstName = firstName;
+    }
+
+    public String getLastName() {
+        return lastName;
+    }
+
+    public void setLastName(String lastName) {
+        this.lastName = lastName;
+    }
 }
--- src/main/java/org/zynminthet/springmvcproject/controllers/HomeController.java
+++ src/main/java/org/zynminthet/springmvcproject/controllers/HomeController.java
@@ -1,33 +1,46 @@
 package org.zynminthet.springmvcproject.controllers;
 
 
+import com.sun.net.httpserver.Request;
+import jakarta.servlet.RequestDispatcher;
+import jakarta.servlet.http.HttpServletRequest;
+import jakarta.servlet.http.HttpServletResponse;
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.stereotype.Controller;
-import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.ui.Model;
+import org.springframework.web.bind.annotation.ModelAttribute;
 import org.springframework.web.bind.annotation.RequestMapping;
+import org.springframework.web.bind.annotation.RequestParam;
+import org.zynminthet.springmvcproject.beans.Student;
+
+import java.io.IOException;
+import java.util.Map;
 
 @Controller
 public class HomeController {
 
+    @Value("#{countryProperties}")
+    private Map<String, String> countryProperties;
 
+    @RequestMapping("/")
+    public void loginPageDamnIt(HttpServletResponse response) throws IOException {
+        response.sendRedirect("login");
+    }
 
-    @RequestMapping("/show-form")
-    public String showForm(){
+    @RequestMapping("/login")
+    public String login(Model model) {
+        model.addAttribute("student", new Student());
+        model.addAttribute("countryOptions", countryProperties);
         return "login";
     }
 
     @RequestMapping("/process-form")
-    public String processForm() {
-        return  "welcome";
+    public String processForm(@ModelAttribute("student")Student student) {
+        return "welcome";
     }
 
-    @RequestMapping("/")
-    public String homePage() {
-        return "home";
+    @RequestMapping("/sort")
+    public String sort(Model model) {
+        return "welcome";
     }
-
-
-
-
-
-
 }
